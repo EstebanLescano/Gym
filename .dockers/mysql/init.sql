6520506dd397f7859d4d2d0a491c7bb1e79d1c38
EstebanBitL <esteban.lescano@bitlogic.io>
2024-10-07 16:32:21 -0300

    metodo create y getall

--- .dockers/mysql/init.sql
+++ .dockers/mysql/init.sql
@@ -6,7 +6,7 @@ CREATE DATABASE IF NOT EXISTS `go_course_user`;
 
 CREATE TABLE `go_course_user`.`users`(
     `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(45) NULL,
-    `last_name` VARCHAR(45) NULL,
-    `email` VARCHAR(45) NULL,
+    `name` VARCHAR(100) NULL,
+    `last_name` VARCHAR(100) NULL,
+    `email` VARCHAR(100) NULL,
     PRIMARY KEY (`id`));
