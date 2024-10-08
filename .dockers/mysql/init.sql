8666b98d0d614a16121fe1e9ffc1b264c994fc54
EstebanBitL <esteban.lescano@bitlogic.io>
2024-10-08 08:02:52 -0300

    metodo create y getall

--- .dockers/mysql/init.sql
+++ .dockers/mysql/init.sql
@@ -2,9 +2,9 @@ SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
 SET @@SESSION.SQL_LOG_BIN=0;
 SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
 
-CREATE DATABASE IF NOT EXISTS `go_course_user`;
+CREATE DATABASE IF NOT EXISTS `go_course_users`;
 
-CREATE TABLE `go_course_user`.`users`(
+CREATE TABLE `go_course_users`.`users`(
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(100) NULL,
     `last_name` VARCHAR(100) NULL,
