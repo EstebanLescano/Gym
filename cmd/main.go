749ede878ef9ca7a6c96997dc92e0fe49d870d44
EstebanBitL <esteban.lescano@bitlogic.io>
2024-10-13 10:01:45 -0300

    cambio usando gin.gonic

--- cmd/main.go
+++ cmd/main.go
@@ -15,8 +15,6 @@ import (
 func main() {
 	_ = godotenv.Load() //esta es una libreria que se usa y asi como esta ya sabe que va y busca el .env donde estan las variables y las ejecuta
 
-	server := http.NewServeMux()
-
 	db, err := bootstrap.NewDB()
 	if err != nil {
 		log.Fatal(err)
@@ -32,9 +30,31 @@ func main() {
 
 	ctx := context.Background()
 
-	handler.NewUserHttpServer(ctx, server, user.MakeEndpoint(ctx, service))
+	h := handler.NewUserHttpServer(user.MakeEndpoint(ctx, service))
 
 	port := os.Getenv("PORT")
 	fmt.Println("Server started at port ", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), server))
+
+	address := fmt.Sprintf("127.0.0.1:%s", port)
+
+	srv := &http.Server{
+		Handler: accessControl(h),
+		Addr:    address,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
+
+// esta funcion se hace para no tener problemas de cors y porder pegarle a la api desde cualquier lado
+func accessControl(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", " GET, POST, PATCH, OPTIONS, HEAD, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Cache-Control, Content-Type,"+
+			"DNT, If-Modified-Since, Keep-Alive, Origin, User-Agent,X-Request-With, Content-Length, Accept-Encoding")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
 }
--- pkg/handler/user.go
+++ pkg/handler/user.go
@@ -1,83 +1,52 @@
 package handler
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/EstebanLescano/Gym/internal/user"
 	"github.com/EstebanLescano/Gym/pkg/transport"
 	"github.com/EstebanLescano/go-fundamentals-response/response"
-	"log"
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
 	"strconv"
 )
 
-func NewUserHttpServer(ctx context.Context, router *http.ServeMux, endpoint user.Endpoint) {
-	router.HandleFunc("/users/", UserServer(ctx, endpoint))
+func NewUserHttpServer(endpoint user.Endpoint) http.Handler {
+	r := gin.Default()
+
+	r.POST("/users", transport.GinServer(
+		transport.Endpoint(endpoint.Create),
+		decodeCreateUser,
+		encodeResponse,
+		encodeError))
+
+	r.GET("/users", transport.GinServer(
+		transport.Endpoint(endpoint.GetAll),
+		decodeGetAllUser,
+		encodeResponse,
+		encodeError))
+
+	r.GET("/users/:id", transport.GinServer(
+		transport.Endpoint(endpoint.Get),
+		decodeGetUser,
+		encodeResponse,
+		encodeError))
+
+	r.PATCH("/users/:id", transport.GinServer(
+		transport.Endpoint(endpoint.Update),
+		decodeUpdateUser,
+		encodeResponse,
+		encodeError))
+	return r
 }
 
-func UserServer(ctx context.Context, endpoint user.Endpoint) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		url := r.URL.Path
-		log.Println(r.Method, ":", url)
-		path, pathSize := transport.Clean(url)
-
-		params := make(map[string]string)
-		if pathSize == 4 && path[2] != "" {
-			params["userID"] = path[2]
-		}
-		params["token"] = r.Header.Get("Authorization")
-
-		tran := transport.New(w, r, context.WithValue(ctx, "params", params))
-
-		var end user.Controller
-		var deco func(ctx context.Context, r *http.Request) (interface{}, error)
-
-		switch r.Method {
-		case http.MethodGet:
-			switch pathSize {
-			case 3:
-				end = endpoint.GetAll
-				deco = decodeGetAllUser
-			case 4:
-				end = endpoint.Get
-				deco = decodeGetUser
-			}
-
-		case http.MethodPost:
-			switch pathSize {
-			case 3:
-				end = endpoint.Create
-				deco = decodeCreateUser
-			}
-		case http.MethodPatch:
-			switch pathSize {
-			case 4:
-				end = endpoint.Update
-				deco = decoUpdateUser
-			}
-		}
-		if end != nil && deco != nil {
-			tran.Server(
-				transport.Endpoint(end),
-				deco,
-				encodeResponse,
-				encodeError)
-		} else {
-			InvalidMethod(w)
-		}
+func decodeGetUser(c *gin.Context) (interface{}, error) {
+	if err := tokenVerify(c.Request.Header.Get("Authorization")); err != nil {
+		return nil, response.Unauthorized(err.Error())
 	}
-}
-
-func decodeGetUser(ctx context.Context, r *http.Request) (interface{}, error) {
-	params, ok := ctx.Value("params").(map[string]string)
-	if !ok || params["userID"] == "" {
-		return nil, errors.New("missing user ID")
-	}
-	id, err := strconv.ParseUint(params["userID"], 10, 64)
+	id, err := strconv.ParseUint(c.Params.ByName("id"), 10, 64)
 	if err != nil {
 		return nil, response.BadRequest(err.Error())
 	}
@@ -86,16 +55,15 @@ func decodeGetUser(ctx context.Context, r *http.Request) (interface{}, error) {
 	}, nil
 }
 
-func decoUpdateUser(ctx context.Context, r *http.Request) (interface{}, error) {
+func decodeUpdateUser(c *gin.Context) (interface{}, error) {
 	var req user.UpdateReq
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		return nil, err
+	if err := json.NewDecoder(c.Request.Body).Decode(&req); err != nil {
+		return nil, response.BadRequest(err.Error())
 	}
-	params := ctx.Value("params").(map[string]string)
-	if err := tokenVerify(params["token"]); err != nil {
+	if err := tokenVerify(c.Request.Header.Get("Authorization")); err != nil {
 		return nil, response.Unauthorized(err.Error())
 	}
-	id, err := strconv.ParseUint(params["userID"], 10, 64)
+	id, err := strconv.ParseUint(c.Params.ByName("id"), 10, 64)
 	if err != nil {
 		return nil, response.BadRequest(err.Error())
 	}
@@ -103,17 +71,20 @@ func decoUpdateUser(ctx context.Context, r *http.Request) (interface{}, error) {
 	return req, nil
 }
 
-func decodeGetAllUser(ctx context.Context, r *http.Request) (interface{}, error) {
+func decodeGetAllUser(c *gin.Context) (interface{}, error) {
+	if err := tokenVerify(c.GetHeader("Authorization")); err != nil {
+		return nil, response.Unauthorized(err.Error())
+	}
 	return nil, nil
 }
 
-func decodeCreateUser(ctx context.Context, r *http.Request) (interface{}, error) {
-	params := ctx.Value("params").(map[string]string)
-	if err := tokenVerify(params["token"]); err != nil {
+func decodeCreateUser(c *gin.Context) (interface{}, error) {
+
+	if err := tokenVerify(c.Request.Header.Get("Authorization")); err != nil {
 		return nil, response.Unauthorized(err.Error())
 	}
 	var req user.CreateReq
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+	if err := json.NewDecoder(c.Request.Body).Decode(&req); err != nil {
 		return nil, response.BadRequest(fmt.Sprintf("fail to decode body '%v'", err.Error()))
 	}
 	return req, nil
@@ -126,43 +97,14 @@ func tokenVerify(token string) error {
 	return nil
 }
 
-func encodeResponse(ctx context.Context, w http.ResponseWriter, resp interface{}) error {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	// Verificar si la respuesta es nil
-	if resp == nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return json.NewEncoder(w).Encode(map[string]string{
-			"error": "No response from server",
-		})
-	}
-	// Intentar convertir la respuesta a response.Response
-	r, ok := resp.(response.Response)
-	if !ok {
-		w.WriteHeader(http.StatusInternalServerError)
-		return json.NewEncoder(w).Encode(map[string]string{
-			"error": "Invalid response type",
-		})
-	}
-	// Si es una respuesta válida, devolverla
-	w.WriteHeader(r.StatusCode())
-	return json.NewEncoder(w).Encode(r)
+func encodeResponse(c *gin.Context, resp interface{}) {
+	r := resp.(response.Response)
+	c.Header("Content-Type", "application/json; charset=utf-8")
+	c.JSON(r.StatusCode(), resp)
 }
 
-func encodeError(ctx context.Context, w http.ResponseWriter, err error) error {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	var resp response.Response
-	if ok := errors.As(err, &resp); !ok {
-		w.WriteHeader(http.StatusInternalServerError)
-		return json.NewEncoder(w).Encode(map[string]string{
-			"error": err.Error(),
-		})
-	}
-	w.WriteHeader(resp.StatusCode())
-	return json.NewEncoder(w).Encode(resp)
-}
-
-func InvalidMethod(w http.ResponseWriter) {
-	status := http.StatusNotFound
-	w.WriteHeader(status)
-	fmt.Fprintf(w, `{"status":%d, "message": "method doesn't exist"}`, status)
+func encodeError(c *gin.Context, err error) {
+	c.Header("Content-Type", "application/json; charset=utf-8")
+	resp := err.(response.Response)
+	c.JSON(resp.StatusCode(), resp)
 }
